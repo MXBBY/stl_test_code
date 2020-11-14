ffd11f9094f103d7af12466ee6d7140051cb066a
mxbby <2621737712@qq.com>
2020-11-14 14:02:43 +0800

    2020-11-14 以完成array测试部分

--- stl_test.cpp
+++ stl_test.cpp
@@ -2,9 +2,7 @@
 #include <cstdlib>
 #include <iostream>
 
-using std::cin;
-using std::cout;
-using std::string;
+using namespace std;
 
 long get_a_target_long(){
     long target=0;
@@ -40,14 +38,36 @@ int compareStrings(const void* a,const void* b){
 #include <ctime>
 #include <cstdlib>
 
+#define ASIZE 500000
+
 namespace jj01{
     void test_array(){
         cout << "\n 以下是对stl中的array进行测试的内容\n";
+
+        array<long,ASIZE> c;
+    
+        clock_t timeStart=clock();
+        for(long i=0;i<ASIZE;i++){
+            c[i]=rand();
+        }
+        cout << "milli-seconds:" << (clock()-timeStart) << endl;
+        cout << "array.size():" << c.size() << endl;
+        cout << "array.front():" << c.front() << endl;
+        cout << "array.back():" << c.back() << endl;
+        cout << "array.data():" << c.data() << endl;
+
+        long target=get_a_target_long();
+
+        timeStart=clock();
+        qsort(c.data(),ASIZE,sizeof(long),compareLongs);
+        long* pItem=(long*)bsearch(&target,(c.data()),ASIZE,sizeof(long),compareLongs);
+        cout << "qsort()+bsearch(),milli-seconds:" << (clock()-timeStart) <<endl;
+        if(pItem!=NULL)    cout<< "fond:"<< *pItem <<endl;
+        else    cout << "Not fond."<<endl;
     }
+}
 
-    array<long,ASIZE> c;
-    
-    clock_t timeStart=clock();
-    
-
+int main(){
+    jj01::test_array();
+    return 0;
 }
