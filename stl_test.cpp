09c94837a16fe311c2171c91d6462140a641a44c
MXBBY <2621737712@qq.com>
2020-11-18 16:28:44 +0800

    2020-11-18 新添vector，list,forward_list的测试。

--- stl_test.cpp
+++ stl_test.cpp
@@ -67,7 +67,161 @@ namespace jj01{
     }
 }
 
+#include <vector>
+#include <stdexcept>
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <ctime>
+#include <algorithm>
+
+namespace jj02{
+    void test_vector(){
+        cout << "\n以下是对array中的vector进行测试的内容。\n";
+
+        vector <string> c;
+        char buf[10];
+
+        clock_t timestart=clock();
+        for(long i=0;i<ASIZE;i++){
+            try{
+               snprintf(buf,10,"%d",rand());
+               c.push_back(string(buf));
+            }
+            catch(exception& p){
+                cout << "i="<< i << p.what() <<endl;
+                abort();
+            }
+        }
+           cout <<"millli-second:" << (clock()-timestart) <<endl;
+           cout <<"vector.size():" << c.size() << endl;
+           cout <<"vector.front():" << c.front() <<endl;
+           cout <<"vector.back():" << c.back() <<endl;
+           cout << "vector.capacity():" << c.capacity()<<endl;
+            
+            string target=get_a_target_string();
+            {
+                timestart=clock();
+                auto pItem=::find(c.begin(),c.end(),target);
+                cout <<"::find(),milli-second:"<<(clock()-timestart) <<endl;
+
+                if(pItem!=c.end()){
+                    cout << "found,"<< *pItem<<endl;
+                }
+                else{
+                    cout << "not found!" <<endl;
+                }
+            }
+            {
+                timestart=clock();
+                sort(c.begin(),c.end());
+                string* pItem=(string*)bsearch(&target,(c.data()),c.size(),sizeof(string),compareStrings);
+                cout << "sort()+bsratch(),milli-second():" << (clock()-timestart)<<endl;
+
+                if(pItem!=NULL){
+                    cout<< "found"<<*pItem<<endl;
+                }
+                else{
+                    cout<<"not found!"<<endl;
+                }
+            }
+    }
+}
+
+#include <list>
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+#include <cstdio>
+#include <stdexcept>
+
+namespace jj03{
+    void test_list(){
+        cout << "\n以下是对stl中list的测试！\n";
+
+        list <string> c;
+        char buf[10];
+
+        clock_t timestart=clock();
+        for(int i=0;i<ASIZE;i++){
+            try{
+                snprintf(buf,10,"%d",rand());
+                c.push_back(string(buf));
+            }
+            catch(exception& p){
+                cout << "i=" <<i <<" "<<p.what() <<endl;
+                abort();
+            }
+        }
+        cout << "milli-second:"<<(clock()-timestart)<<endl;
+        cout << "list.size():" << c.size()<<endl;
+        cout << "list.front():" <<c.front()<<endl;
+        cout << "list.back():" <<c.back()<<endl;
+        cout<< "list.max_size():" <<c.max_size()<<endl;
+
+        string target=get_a_target_string();
+        timestart=clock();
+        auto pItem=::find(c.begin(),c.end(),target);
+        cout<<"::find(),milli-second():"<<(clock()-timestart)<<endl;
+
+        if(pItem!=c.end()){
+            cout<< "found,"<<*pItem <<endl;
+        }
+        else{
+            cout<<"not found!"<<endl;
+        }
+
+        timestart=clock();
+        c.sort();
+        cout <<"sort(),milli-second():"<<(clock()-timestart)<<endl;
+    }
+}
+
+#include <forward_list>
+
+namespace jj04{
+    void test_forward_list(){
+        cout<<"以下是对stl中的foraward_list进场测试的内容。";
+
+        forward_list<string> c;
+        char buf[10];
+
+        clock_t timestart=clock();
+        for(int i=0;i<ASIZE;i++){
+            try{
+                snprintf(buf,10,"%d",rand());
+                c.push_front(string(buf));
+            }
+            catch(exception& p){
+                cout <<"i="<<i<<" "<<p.what()<<endl;
+                abort();
+            }
+        }
+        cout<<"milli-second:"<<(clock()-timestart)<<endl;
+        cout<<"forward-list.max_sieze():"<<c.max_size()<<endl;
+        cout<<"forward-list.front():"<<c.front()<<endl;
+
+        string target=get_a_target_string();
+        timestart=clock();
+        auto pItem=::find(c.begin(),c.end(),target);
+        cout<<"::find(),milli-second:"<<(clock()-timestart)<<endl;
+
+        if(pItem!=c.end()){
+            cout<<"found,"<<*pItem<<endl;
+        }
+        else{
+            cout<<"not found!"<<endl;
+        }
+
+        timestart=clock();
+        c.sort();
+        cout<<"milli-second:"<<(clock()-timestart)<<endl;
+    }
+}
+/*
 int main(){
-    jj01::test_array();
+    jj04::test_forward_list();
     return 0;
 }
+*/
